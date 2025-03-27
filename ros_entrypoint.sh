d61a980f30a85f3658ee935284ab094d184edecb
Jacob Wilde <71982105+wildildee@users.noreply.github.com>
2025-03-27 14:56:27 -0400

    Update ros_entrypoint.sh

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -1,7 +1,8 @@
+. /opt/ros/humble/setup.bash
+
 #Build 
 cd ~/ros2_ws
 colcon build
 # Run
-source /opt/ros/humble/setup.bash
-source install/local_setup.bash 
+. install/local_setup.bash 
 ros2 run battery_monitor battery_monitor
